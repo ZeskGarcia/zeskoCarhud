19b8f515904f473fd4b05a52e2a4fdc9b7b19d12
Zesk García Rodríguez Salaberría <116458300+ZeskGarcia@users.noreply.github.com>
2022-11-05 12:56:48 +0100

    Add files via upload

---
+++ Config.lua
@@ -0,0 +1,6 @@
+Config = {
+    UseKMH = true, --Check this if you want to use KMH instead of MPH
+    Texts = {
+        ['notInCar'] = "You are not in a car, so you can't use this command"
+    }
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'ZeskoGarcía'
+website 'http://store.zeskoogarcia.com'
+lua54 "true"
+
+client_scripts {
+    'Client/Main.lua',
+    'Config.lu*'
+}
+escrow_ignore {
+    'Config.lua',
+    'nui/nui.html',
+    'nui/images/gearbox.png'
+}
+ui_page 'nui/nui.html'
+files {
+    'nui/nui.html',
+    'nui/css/*.css',
+    'nui/js/*.js',
+    'nui/images/*.png'
+}
