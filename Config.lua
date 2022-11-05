eae5c54eedf65f6996834dbeef376c67ac8c1ba0
Zesk García Rodríguez Salaberría <116458300+ZeskGarcia@users.noreply.github.com>
2022-11-05 13:15:37 +0100

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,5 +1,6 @@
 Config = {
     UseKMH = true, --Check this if you want to use KMH instead of MPH
+    DEBUG = false, -- Check this if you want to get DEBUG logs
     Texts = {
         ['notInCar'] = "You are not in a car, so you can't use this command"
     }
