03b5c00417faba8f713d65cf2eb02570e4c4a7c8
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2023-06-17 11:27:11 +0200

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,7 +1,12 @@
 Config = {
     UseKMH = true, --Check this if you want to use KMH instead of MPH
     DEBUG = false, -- Check this if you want to get DEBUG logs
+    CustomNotify = false, -- Check this if you want to add your custom notifications.
     Texts = {
         ['notInCar'] = "You are not in a car, so you can't use this command"
     }
 }
+
+CustomNotification = function(text)
+    -- Add here the code for your notifications.
+end
